8df1da430b7414a3e76c4874aef31bf5ac07fc7f
miaoihan <izhanghan@outlook.com>
2016-05-02 00:32:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+Title         : fun脚本语言
+Author        : han
+Logo          : False
+
+[TITLE]
+
+# fun
+
+基于java的一个脚本语言.
+
+* 语言设计.
+* 正在实现当中
+*
+
+Enjoy!
+
---
+++ src/fun/Token.java
@@ -0,0 +1,11 @@
+package fun;
+
+/**
+ * time: 16-5-2.
+ * author: han
+ */
+public class Token {
+    public static void main(String[] args) {
+        System.out.println("Token.main");
+    }
+}
