e17ef5726f86485f5dde4ce2cd04aad769021e65
miaoihan <izhanghan@outlook.com>
2016-05-02 00:48:17 +0800

    分割单词

--- src/fun/Token.java
+++ src/fun/Token.java
@@ -4,8 +4,24 @@ package fun;
  * time: 16-5-2.
  * author: han
  */
-public class Token {
-    public static void main(String[] args) {
-        System.out.println("Token.main");
+public abstract class Token {
+    public static final Token EOF = new Token(-1){};    //end of file
+    public static final String ROL = "\\n";             //end of line
+    private int lineNumber;
+    protected Token(int line){
+        lineNumber = line;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+    public boolean isIdentifier(){ return false; }
+    public boolean isNumber(){ return false; }
+    public boolean isString(){ return false; }
+    public int getNumber(){  throw new FunException("not number token");  }
+
+    public String getText() {
+        return "";
     }
 }
+
