46ec664b7ca54aa02508805f57c984cd0303cad3
miaoihan <izhanghan@outlook.com>
2016-05-12 22:49:51 +0800

    抽象语法树

---
+++ src/fun/ast/ASTLeaf.java
@@ -0,0 +1,56 @@
+package fun.ast;
+
+import fun.Token;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * 叶子节点类。继承自ASTree
+ * time: 16-5-12.
+ * author: han
+ */
+public class ASTLeaf extends ASTree {
+
+    private static ArrayList<ASTree> empty = new ArrayList<>();
+    protected Token token;
+    public ASTLeaf(Token t) {token = t;}
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    @Override
+    public ASTree child(int i) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    /**
+     * 叶子节点没有子节点 返回0
+     * @return 0
+     */
+    @Override
+    public int numChildren() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<ASTree> children() {
+        return empty.iterator();
+    }
+
+    @Override
+    public String location() {
+        return "at line " + token.getLineNumber();
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    @Override
+    public String toString() {
+        return token.getText();
+    }
+}
